6fa091a4fc6b98c32c0e4b60899bc791d066f6ad
Junichi Kato <j5ik2o@gmail.com>
2020-03-22 19:01:20 +0900

    fix resource names.

--- terraform/gatling/output.tf
+++ terraform/gatling/output.tf
@@ -1,29 +1,29 @@
 output "gatling_ecs_cluster_name" {
-  value = aws_ecs_cluster.ecs_cluster.name
+  value = aws_ecs_cluster.ecs_cluster[0].name
 }
 
 output "gatling_runner_ecr_arn" {
-  value =  aws_ecr_repository.gatling_runner_ecr.arn
+  value = aws_ecr_repository.gatling_runner_ecr[0].arn
 }
 
 output "gatling_runner_ecr_repository_url" {
-  value =  aws_ecr_repository.gatling_runner_ecr.repository_url
+  value = aws_ecr_repository.gatling_runner_ecr[0].repository_url
 }
 
 output "gatling_s3_reporter_ecr_arn" {
-  value =  aws_ecr_repository.gatling_s3_reporter_ecr.arn
+  value = aws_ecr_repository.gatling_s3_reporter_ecr[0].arn
 }
 
 output "gatling_s3_reporter_ecr_repository_url" {
-  value =  aws_ecr_repository.gatling_s3_reporter_ecr.repository_url
+  value = aws_ecr_repository.gatling_s3_reporter_ecr[0].repository_url
 }
 
 output "gatling_aggregate_runner_ecr_arn" {
-  value =  aws_ecr_repository.gatling_aggregate_runner_ecr.arn
+  value = aws_ecr_repository.gatling_aggregate_runner_ecr[0].arn
 }
 
 output "gatling_aggregate_runner_ecr_repository_url" {
-  value =  aws_ecr_repository.gatling_aggregate_runner_ecr.repository_url
+  value = aws_ecr_repository.gatling_aggregate_runner_ecr[0].repository_url
 }
 
 
