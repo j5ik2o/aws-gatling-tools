2f0628532765c3d5ebb6c7755b83330bb77aff1a
Junichi Kato <j5ik2o@gmail.com>
2020-03-23 03:52:53 +0900

    refactor

--- api-server/src/main/scala/example/api/server/ApiServer.scala
+++ api-server/src/main/scala/example/api/server/ApiServer.scala
@@ -5,12 +5,12 @@ import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
 
-import scala.concurrent.Await
+import scala.concurrent.{Await, ExecutionContextExecutor}
 import scala.concurrent.duration._
 
 object ApiServer extends App {
-  implicit val system = ActorSystem("my-system")
-  implicit val executionContext = system.dispatcher
+  implicit val system: ActorSystem = ActorSystem("api-server")
+  implicit val executionContext: ExecutionContextExecutor = system.dispatcher
 
   val route =
     path("hello") {
@@ -19,7 +19,9 @@ object ApiServer extends App {
       }
     }
 
-  val bindingFuture = Http().bindAndHandle(route, "0.0.0.0", 80).map {
+  val httpPort = system.settings.config.getInt("http-port")
+
+  val bindingFuture = Http().bindAndHandle(route, "0.0.0.0", httpPort).map {
     serverBinding =>
       system.log.info(s"Server online at ${serverBinding.localAddress}")
       serverBinding
